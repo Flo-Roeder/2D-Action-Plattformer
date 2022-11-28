bbe367e149c6a464bac49da58de4f9cf3a5861b8
Flo-Roeder <flowrain@gmx.de>
2022-11-28 16:03:37 +0100

    first build

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UIManager : MonoBehaviour
 {
@@ -44,4 +45,17 @@ public class UIManager : MonoBehaviour
 
         tmpText.text = healthRestored.ToString();
     }
+
+    public void OnExitGame(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+        #if (UNITY_EDITOR)
+            UnityEditor.EditorApplication.isPlaying=false;
+        #elif (UNITY_STANDALONE)
+            Application.Quit();
+        #endif
+        }
+    }
+
 }
