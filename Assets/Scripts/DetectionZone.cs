b82e33efc1000acffc304a5afc4a1717749dcc7d
Flo-Roeder <flowrain@gmx.de>
2022-11-28 17:07:35 +0100

    final push?

--- Assets/Scripts/DetectionZone.cs
+++ Assets/Scripts/DetectionZone.cs
@@ -7,7 +7,7 @@ public class DetectionZone : MonoBehaviour
 {
     public UnityEvent noCollidersRemain;
 
-    public List<Collider2D> detectedColliders= new List<Collider2D>();
+    public List<Collider2D> detectedColliders= new();
     Collider2D col;
 
     private void Awake()
